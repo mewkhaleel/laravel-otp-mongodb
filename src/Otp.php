310b5b430c29238017dc205993496ae6145c983b
mewkhaleel <112711632+mewkhaleel@users.noreply.github.com>
2022-09-03 10:49:40 +0400

    Update Otp.php

--- src/Otp.php
+++ src/Otp.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Ichtrojan\Otp;
+namespace mewkhaleel\Otp;
 
 use Carbon\Carbon;
-use Ichtrojan\Otp\Models\Otp as Model;
+use mewkhaleel\Otp\Models\Otp as Model;
 use Illuminate\Support\Facades\Facade;
 
 class Otp extends Facade
